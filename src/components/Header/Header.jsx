d46c4bcabb4ac1a828f80b07f8ca17f7188a61e8
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2024-07-02 22:58:59 -0400

    Move header to own folder with corresponding css file, begin work on footer

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import './header.css';
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import GenesisBlackBelt from '../../app/assets/genesis-bjj-center-wendel-gabetta.png';
+
+const Header = () => {
+    return (
+        <Container>
+            <Navbar sticky='top' expand='lg' className='bg-navbar'>
+                <Navbar.Brand href='#home'>
+                    <img href='#home' src={GenesisBlackBelt} aria-label='Genesis Black Belt Logo' />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse>
+                    <Nav>
+                        <Nav.Link href='#home'>Home</Nav.Link>
+                        <Nav.Link href='#about'>About</Nav.Link>
+                        <Nav.Link href='#schedule'>Schedule</Nav.Link>
+                        <Nav.Link href='#contact'>Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </Container>
+    );
+}
+
+export default Header;
