26fc70c5e5420e670a3b4ad9b8dd8c83d71392fb
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2024-08-07 22:47:50 -0400

    Add aria labels and _blank to targets to open new tabs

--- src/components/FooterComponent/Footer.jsx
+++ src/components/FooterComponent/Footer.jsx
@@ -25,18 +25,20 @@ const Footer = () => {
                                 <a href='#about'>About</a>
                                 <br />
                                 <a href='#schedule'>Schedule</a>
+                                <br />
+                                <a target='_blank' href='https://www.google.com/maps/place/Genesis+Brazilian+Jiu+Jitsu+Center/@27.2706485,-80.2990699,15z/data=!4m6!3m5!1s0x88dee917c9937f0b:0x71e2bdcefa83057b!8m2!3d27.2706485!4d-80.2990699!16s%2Fg%2F11nfpmmy_2?entry=ttu'>Location</a>
                             </Col>
                             <Col lg={2} className='pt-2 pt-lg-0'>
                                 <h3>Contact</h3>
-                                <a href='tel:+17727662313'>
+                                <a href='tel:+17727662313' aria-label='Phone'>
                                     <i className='fa fa-phone' /> (772) 766-2313
                                 </a>
                                 <br />
-                                <a href='tel:+17727662315'>
+                                <a href='tel:+17727662315' aria-label='Phone'>
                                     <i className='fa fa-phone' /> (772) 766-2315
                                 </a>
                                 <br />
-                                <a href='mailto:genesisbjj20@gmail.com'>
+                                <a href='mailto:genesisbjj20@gmail.com' aria-label='Email'>
                                     <i className='fa fa-envelope-o' /> genesisbjj20@gmail.com
                                 </a>
                             </Col>
@@ -44,10 +46,10 @@ const Footer = () => {
                                 <h3>Social</h3>
                                 <Row>
                                     <Col className='footer-social-col'>
-                                        <a href='https://www.instagram.com/genesisbjjcenter/'>
+                                        <a target='_blank' href='https://www.instagram.com/genesisbjjcenter/' aria-label='Instagram'>
                                             <i className='fa fa-instagram fa-2x' style={{ color: '#000000' }}></i>
                                         </a>
-                                        <a href='https://www.facebook.com/genesisbjjcenter'>
+                                        <a target='_blank' href='https://www.facebook.com/genesisbjjcenter' aria-label='Facebook'>
                                             <i className='fa fa-facebook fa-2x' style={{ color: '#000000' }}></i>
                                         </a>
                                     </Col>
