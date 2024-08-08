6bd3fb6a4836535fd0e4e5c119596c81d9b77380
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2024-08-07 22:09:29 -0400

    Adjust Footer formatting and website title and tab icon

--- src/components/FooterComponent/Footer.jsx
+++ src/components/FooterComponent/Footer.jsx
@@ -11,41 +11,47 @@ const Footer = () => {
         <footer id='footer'>
             <Container fluid>
                 <Row>
-                    <Col md='4'>
-                        <a href='#home'>
-                            <img src={GenesisLogo} className='footer-logo' />
-                        </a>
-                    </Col>
                     <Col>
-                        <h3>Gym</h3>
-                        <a href='#about'>About</a> 
-                        <br/>
-                        <a href='#schedule'>Schedule</a>
-                    </Col>
-                    <Col>
-                        <h3>Contact</h3>
-                        <a href='tel:+17727662313'>
-                            <i className='fa fa-phone' /> (772) 766-2313
-                        </a>
-                        <br />
-                        <a href='tel:+17727662315'>
-                            <i className='fa fa-phone' /> (772) 766-2315
-                        </a>
-                        <br />
-                        <a href='mailto:genesisbjj20@gmail.com'>
-                            <i className='fa fa-envelope-o' /> genesisbjj20@gmail.com
-                        </a>
-                    </Col>
-                    <Col>
-                        <h3>Social</h3>
-                        <Row>
-                            <Col className='footer-social-col'>
-                                <a href='https://www.instagram.com/genesisbjjcenter/'>
-                                    <i className='fa fa-instagram fa-2x' style={{color: '#000000'}}></i>
+                        <Row className='p-3'>
+                            <Col>
+                                <a href='#home'>
+                                    <img src={GenesisLogo} className='footer-logo' />
                                 </a>
-                                <a href='https://www.facebook.com/genesisbjjcenter'>
-                                    <i className='fa fa-facebook fa-2x' style={{color: '#000000'}}></i>
+                            </Col>
+                        </Row>
+                        <Row className='justify-content-center'>
+                            <Col lg={2} className='pt-2 pt-lg-0'>
+                                <h3>Gym</h3>
+                                <a href='#about'>About</a>
+                                <br />
+                                <a href='#schedule'>Schedule</a>
+                            </Col>
+                            <Col lg={2} className='pt-2 pt-lg-0'>
+                                <h3>Contact</h3>
+                                <a href='tel:+17727662313'>
+                                    <i className='fa fa-phone' /> (772) 766-2313
                                 </a>
+                                <br />
+                                <a href='tel:+17727662315'>
+                                    <i className='fa fa-phone' /> (772) 766-2315
+                                </a>
+                                <br />
+                                <a href='mailto:genesisbjj20@gmail.com'>
+                                    <i className='fa fa-envelope-o' /> genesisbjj20@gmail.com
+                                </a>
+                            </Col>
+                            <Col lg={2} className='pt-2 pt-lg-0'>
+                                <h3>Social</h3>
+                                <Row>
+                                    <Col className='footer-social-col'>
+                                        <a href='https://www.instagram.com/genesisbjjcenter/'>
+                                            <i className='fa fa-instagram fa-2x' style={{ color: '#000000' }}></i>
+                                        </a>
+                                        <a href='https://www.facebook.com/genesisbjjcenter'>
+                                            <i className='fa fa-facebook fa-2x' style={{ color: '#000000' }}></i>
+                                        </a>
+                                    </Col>
+                                </Row>
                             </Col>
                         </Row>
                     </Col>
